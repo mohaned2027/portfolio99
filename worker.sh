0f3f9e1e31d1d22a1369b271821a042cde23c11a
Mohanad <mohandahmed015529.222.00@gmail.com>
2026-01-30 08:49:41 +0200

    first commit

---
+++ worker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -e
+
+echo "=============================="
+echo "👷 Starting Queue Worker"
+echo "=============================="
+
+cd /var/www
+
+chmod -R 775 storage bootstrap/cache || true
+
+# مهم جدًا عشان ياخد ENV الجديد
+php artisan optimize:clear || true
+php artisan queue:restart || true
+
+exec php artisan queue:work --sleep=3 --tries=1 --timeout=180
