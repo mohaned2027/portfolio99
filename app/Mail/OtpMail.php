e4a7c77380cb920e45b2247b7b9c8440df19a8f3
mohaned2027 <mohabmohan800@gmail.com>
2026-01-30 08:36:55 +0200

    Update OtpMail.php

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class OtpMail extends Mailable 
+class OtpMail extends Mailable implements ShouldQueue 
 {
     use Queueable, SerializesModels;
 
