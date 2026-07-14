173b3b70263da516def17c208fde7c3c9bb754ea
Mohaned Ahmed <mohandahmed015529.222.00@gmail.com>
2026-07-14 19:06:04 +0300

    Remove Laravel Boost

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 composer require symfony/brevo-mailer
 composer require symfony/http-client
 composer require resend/resend-laravel
+
+
--- routes/api.php
+++ routes/api.php
@@ -30,9 +30,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 // Portfolio
